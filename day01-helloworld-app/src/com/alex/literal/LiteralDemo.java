2613b5c20828c9238dd4c54fc0f7a2a58d66b5dd
alexlee <alexlee910116@gmail.com>
2023-12-08 11:22:03 +0900

    learn java

---
+++ day01-helloworld-app/src/com/alex/literal/LiteralDemo.java
@@ -0,0 +1,13 @@
+package com.alex.literal;
+
+public class LiteralDemo {
+    public static void main(String[] args) {
+        System.out.println(666);
+        System.out.println('a');
+        System.out.println("aa");
+        System.out.println(" ");
+        System.out.println("......");
+        System.out.println('\n');
+        System.out.println("......");
+    }
+}
---
+++ day01-helloworld-app/src/com/alex/note/Note.java
@@ -0,0 +1,15 @@
+package com.alex.note;
+
+/**
+ *Target
+ */
+
+public class Note {
+    public static void main(String[] args) {
+        // print content
+        System.out.println("Hello World");
+        /*
+        comment
+         */
+    }
+}
